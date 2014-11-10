93f5c48eb5b5a9dab3a290fb968179a6727d25c0
George Dietz <dietzgeorge@gmail.com>
2014-11-09 21:38:14 -0500

    refine

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 sandbox-py2app-pyqt-rpy2
 ========================
 
-Example of using pyqt, py2app and rpy2 together
+Example of using pyqt, py2app and rpy2 together to make a mac app using R on the
+backend to perform a calculation.
--- main.py
+++ main.py
@@ -1,22 +1,9 @@
-import os, sys
+import sys
 
 from PyQt4 import QtCore, QtGui
 from PyQt4.Qt import *
 
 import ui_form
-
-oldpath = os.environ["PATH"]
-cwd = os.getcwd()
-rpath = os.path.join(cwd, "..","Resources", "Resources")
-os.environ["PATH"] = os.path.join(rpath, "bin") + os.pathsep + oldpath
-print("new path is: %s" % os.environ["PATH"])
-
-os.environ["R"] = \
-    os.path.join(cwd, rpath, "bin")
-
-os.environ["R_HOME"] = \
-    os.path.join(cwd, rpath)
-
 
 from rpy2 import robjects as ro
 
@@ -29,12 +16,13 @@ class HelloWorldDlg(QtGui.QDialog, ui_form.Ui_Dialog):
         
         self.result_lbl.setText("Results is %d" % ro.r("2+3")[0])
         
-
-
-if __name__ == '__main__':
-    #pass        
+def start():
     app = QApplication(sys.argv)
     form = HelloWorldDlg()
     form.show()
     form.raise_()
     app.exec_()
+
+if __name__ == '__main__':
+    #pass        
+    start()
